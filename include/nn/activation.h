193f33c448210939a49772977cfa8bece048b259
yonko-sergiy <yonko.sergiy@proton.me>
2026-05-22 12:58:50 +0300

    added more activation functions

--- include/nn/activation.h
+++ include/nn/activation.h
@@ -24,9 +24,43 @@ private:
 }; // end Activation -- base class
 
 
+// Identity
+class Identity : public Activation {
+public:
+  Eigen::MatrixXd function(Eigen::MatrixXd X) const override;
+  Eigen::MatrixXd gradient(Eigen::MatrixXd X) const override;
+
+  static std::shared_ptr<Identity> create();
+
+private:
+  friend class boost::serialization::access;
+  template<class Archive>
+  void serialize(Archive & ar, const unsigned int version) {
+    ar & BOOST_SERIALIZATION_BASE_OBJECT_NVP(Activation);
+  };
+}; // end Identity
+
+
+// Tanh
+class Tanh : public Activation {
+public:
+  Eigen::MatrixXd function(Eigen::MatrixXd X) const override;
+  Eigen::MatrixXd gradient(Eigen::MatrixXd X) const override;
+
+  static std::shared_ptr<Tanh> create();
+
+private:
+  friend class boost::serialization::access;
+  template<class Archive>
+  void serialize(Archive & ar, const unsigned int version) {
+    ar & BOOST_SERIALIZATION_BASE_OBJECT_NVP(Activation);
+  };
+}; // end Tanh
+
+
+// Sigmoid
 class Sigmoid : public Activation {
 public:
-
   Eigen::MatrixXd function(Eigen::MatrixXd X) const override;
   Eigen::MatrixXd gradient(Eigen::MatrixXd X) const override;
 
@@ -39,6 +73,91 @@ private:
     ar & BOOST_SERIALIZATION_BASE_OBJECT_NVP(Activation);
   };
 }; // end Sigmoid
+
+
+// Softsign
+class Softsign : public Activation {
+public:
+  Eigen::MatrixXd function(Eigen::MatrixXd X) const override;
+  Eigen::MatrixXd gradient(Eigen::MatrixXd X) const override;
+
+  static std::shared_ptr<Softsign> create();
+
+private:
+  friend class boost::serialization::access;
+  template<class Archive>
+  void serialize(Archive & ar, const unsigned int version) {
+    ar & BOOST_SERIALIZATION_BASE_OBJECT_NVP(Activation);
+  };
+}; // end Softsign
+
+
+// Softplus
+class Softplus : public Activation {
+public:
+  Eigen::MatrixXd function(Eigen::MatrixXd X) const override;
+  Eigen::MatrixXd gradient(Eigen::MatrixXd X) const override;
+
+  static std::shared_ptr<Softplus> create();
+
+private:
+  friend class boost::serialization::access;
+  template<class Archive>
+  void serialize(Archive & ar, const unsigned int version) {
+    ar & BOOST_SERIALIZATION_BASE_OBJECT_NVP(Activation);
+  };
+}; // end Softplus
+
+
+// SiLU
+class SiLU : public Activation {
+public:
+  Eigen::MatrixXd function(Eigen::MatrixXd X) const override;
+  Eigen::MatrixXd gradient(Eigen::MatrixXd X) const override;
+
+  static std::shared_ptr<SiLU> create();
+
+private:
+  friend class boost::serialization::access;
+  template<class Archive>
+  void serialize(Archive & ar, const unsigned int version) {
+    ar & BOOST_SERIALIZATION_BASE_OBJECT_NVP(Activation);
+  };
+}; // end SiLU
+
+
+// Gaussian
+class Gaussian : public Activation {
+public:
+  Eigen::MatrixXd function(Eigen::MatrixXd X) const override;
+  Eigen::MatrixXd gradient(Eigen::MatrixXd X) const override;
+
+  static std::shared_ptr<Gaussian> create();
+
+private:
+  friend class boost::serialization::access;
+  template<class Archive>
+  void serialize(Archive & ar, const unsigned int version) {
+    ar & BOOST_SERIALIZATION_BASE_OBJECT_NVP(Activation);
+  };
+}; // end Gaussian
+
+
+// Sinusoid
+class Sinusoid : public Activation {
+public:
+  Eigen::MatrixXd function(Eigen::MatrixXd X) const override;
+  Eigen::MatrixXd gradient(Eigen::MatrixXd X) const override;
+
+  static std::shared_ptr<Sinusoid> create();
+
+private:
+  friend class boost::serialization::access;
+  template<class Archive>
+  void serialize(Archive & ar, const unsigned int version) {
+    ar & BOOST_SERIALIZATION_BASE_OBJECT_NVP(Activation);
+  };
+}; // end Sinusoid
 
 } // namespace Activations
 } // namespace NN
--- src/nn/activation.cpp
+++ src/nn/activation.cpp
@@ -4,7 +4,14 @@
 #include "nn/activation.h"
 
 
+BOOST_CLASS_EXPORT(NN::Activations::Identity)
+BOOST_CLASS_EXPORT(NN::Activations::Tanh)
 BOOST_CLASS_EXPORT(NN::Activations::Sigmoid)
+BOOST_CLASS_EXPORT(NN::Activations::Softsign)
+BOOST_CLASS_EXPORT(NN::Activations::Softplus)
+BOOST_CLASS_EXPORT(NN::Activations::SiLU)
+BOOST_CLASS_EXPORT(NN::Activations::Gaussian)
+BOOST_CLASS_EXPORT(NN::Activations::Sinusoid)
 
 namespace NN {
 namespace Activations {
@@ -17,6 +24,51 @@ Activation::function(Eigen::MatrixXd X) const { return X; };
 Eigen::MatrixXd
 Activation::gradient(Eigen::MatrixXd X) const { return X; };
 // end Activation -- base class
+
+
+// Identity
+std::shared_ptr<Identity>
+Identity::create() { return std::make_shared<Identity>(); };
+
+Eigen::MatrixXd
+Identity::function(Eigen::MatrixXd X) const {
+  Eigen::MatrixXd tmp_output = X.unaryExpr([](double x) {
+    return x;
+  });
+  return tmp_output;
+};
+
+Eigen::MatrixXd
+Identity::gradient(Eigen::MatrixXd X) const {
+  Eigen::MatrixXd tmp_output = X.unaryExpr([](double x) {
+    return x/x; // 1
+  });
+  return tmp_output;
+};
+// end Identity
+
+
+// Tanh
+std::shared_ptr<Tanh>
+Tanh::create() { return std::make_shared<Tanh>(); };
+
+Eigen::MatrixXd
+Tanh::function(Eigen::MatrixXd X) const {
+  Eigen::MatrixXd tmp_output = X.unaryExpr([](double x) {
+    return (std::exp(x) - std::exp(-x)) /
+           (std::exp(x) + std::exp(-x));
+  });
+  return tmp_output;
+};
+
+Eigen::MatrixXd
+Tanh::gradient(Eigen::MatrixXd X) const {
+  Eigen::MatrixXd tmp_output = function(X).unaryExpr([](double x) {
+    return x * (1 - x); // 1
+  });
+  return tmp_output;
+};
+// end Tanh
 
 
 // Sigmoid
@@ -39,6 +91,117 @@ Sigmoid::gradient(Eigen::MatrixXd X) const {
   return tmp_output;
 };
 // end Sigmoid
+
+
+// Softsign
+std::shared_ptr<Softsign>
+Softsign::create() { return std::make_shared<Softsign>(); };
+
+Eigen::MatrixXd
+Softsign::function(Eigen::MatrixXd X) const {
+  Eigen::MatrixXd tmp_output = X.unaryExpr([](double x) {
+    return x / (1.0 + std::abs(x));
+  });
+  return tmp_output;
+};
+
+Eigen::MatrixXd
+Softsign::gradient(Eigen::MatrixXd X) const {
+  Eigen::MatrixXd tmp_output = X.unaryExpr([](double x) {
+    return 1.0 / std::pow(1.0 + std::abs(x), 2);
+  });
+  return tmp_output;
+};
+// end Softsign
+
+
+// Softplus
+std::shared_ptr<Softplus>
+Softplus::create() { return std::make_shared<Softplus>(); };
+
+Eigen::MatrixXd
+Softplus::function(Eigen::MatrixXd X) const {
+  Eigen::MatrixXd tmp_output = X.unaryExpr([](double x) {
+    return std::log(1.0 + std::exp(x));
+  });
+  return tmp_output;
+};
+
+Eigen::MatrixXd
+Softplus::gradient(Eigen::MatrixXd X) const {
+  Eigen::MatrixXd tmp_output = X.unaryExpr([](double x) {
+    return 1.0 / (1.0 + std::exp(-x));
+  });
+  return tmp_output;
+};
+// end Softplus
+
+
+// SiLU
+std::shared_ptr<SiLU>
+SiLU::create() { return std::make_shared<SiLU>(); };
+
+Eigen::MatrixXd
+SiLU::function(Eigen::MatrixXd X) const {
+  Eigen::MatrixXd tmp_output = X.unaryExpr([](double x) {
+    return x / (1.0 + std::exp(-x));
+  });
+  return tmp_output;
+};
+
+Eigen::MatrixXd
+SiLU::gradient(Eigen::MatrixXd X) const {
+  Eigen::MatrixXd tmp_output = X.unaryExpr([](double x) {
+    return (1 + std::exp(-x) + x * std::exp(-x)) /
+           pow(1 + std::exp(-x), 2);
+  });
+  return tmp_output;
+};
+// end SiLU
+
+
+// Gaussian
+std::shared_ptr<Gaussian>
+Gaussian::create() { return std::make_shared<Gaussian>(); };
+
+Eigen::MatrixXd
+Gaussian::function(Eigen::MatrixXd X) const {
+  Eigen::MatrixXd tmp_output = X.unaryExpr([](double x) {
+    return std::exp(-std::pow(x, 2));
+  });
+  return tmp_output;
+};
+
+Eigen::MatrixXd
+Gaussian::gradient(Eigen::MatrixXd X) const {
+  Eigen::MatrixXd tmp_output = X.unaryExpr([](double x) {
+    return -2 * x * std::exp(-std::pow(x, 2));
+  });
+  return tmp_output;
+};
+// end Gaussian
+
+
+// Sinusoid
+std::shared_ptr<Sinusoid>
+Sinusoid::create() { return std::make_shared<Sinusoid>(); };
+
+Eigen::MatrixXd
+Sinusoid::function(Eigen::MatrixXd X) const {
+  Eigen::MatrixXd tmp_output = X.unaryExpr([](double x) {
+    return std::sin(x);
+  });
+  return tmp_output;
+};
+
+Eigen::MatrixXd
+Sinusoid::gradient(Eigen::MatrixXd X) const {
+  Eigen::MatrixXd tmp_output = X.unaryExpr([](double x) {
+    return std::cos(x);
+  });
+  return tmp_output;
+};
+// end Sinusoid
 
 } // namespace Activations
 } // namespace NN
